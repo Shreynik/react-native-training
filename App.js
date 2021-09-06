abac028b3229021c1166ca6e30844d5e488ad2d7
shreynik <shreynik_bt2k14@dtu.ac.in>
2021-09-06 23:40:57 +0530

    items done

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, TouchableOpacity, View, Image } from 'react-native';
 import { ColorsList } from './constants/colors';
 
 const deleteIcon = require('./images/delete.png');
+// import Items from './Items';
 
 const App = () => {
   const [rows, setRows] = useState([]);
@@ -25,61 +26,64 @@ const App = () => {
   };
 
   return (
-    <View style={{ flex: 1, margin: 10, padding: 20 }}>
-      <ScrollView>
-        <Text style={{ margin: 10 }}>Color List</Text>
-        <View>
-          {rows.map((r, i) => {
-            return (
-              <View
-                key={`${i}-${r}`}
-                style={{ flexDirection: 'row', margin: 10 }}
-              >
+    <>
+      {/* <Items /> */}
+      <View style={{ flex: 1, margin: 10, padding: 20 }}>
+        <ScrollView>
+          <Text style={{ margin: 10 }}>Color List</Text>
+          <View>
+            {rows.map((r, i) => {
+              return (
                 <View
-                  style={{
-                    flex: 1,
-                    backgroundColor: r,
-                    height: 20,
-                    borderRadius: 5,
-                  }}
-                />
-                <TouchableOpacity onPress={() => removeRow(r)}>
-                  <Image
-                    style={{ width: 20, height: 20, marginLeft: 10 }}
-                    source={deleteIcon}
+                  key={`${i}-${r}`}
+                  style={{ flexDirection: 'row', margin: 10 }}
+                >
+                  <View
+                    style={{
+                      flex: 1,
+                      backgroundColor: r,
+                      height: 20,
+                      borderRadius: 5,
+                    }}
+                  />
+                  <TouchableOpacity onPress={() => removeRow(r)}>
+                    <Image
+                      style={{ width: 20, height: 20, marginLeft: 10 }}
+                      source={deleteIcon}
+                    />
+                  </TouchableOpacity>
+                </View>
+              );
+            })}
+          </View>
+        </ScrollView>
+        <View style={{ marginTop: 10 }}>
+          <View
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'row',
+            }}
+          >
+            {ColorsList.map((color) => {
+              return (
+                <TouchableOpacity key={color} onPress={() => addRow(color)}>
+                  <View
+                    style={{
+                      height: 30,
+                      backgroundColor: color,
+                      width: 30,
+                      borderRadius: 20,
+                      margin: 8,
+                    }}
                   />
                 </TouchableOpacity>
-              </View>
-            );
-          })}
-        </View>
-      </ScrollView>
-      <View style={{ marginTop: 10 }}>
-        <View
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'row',
-          }}
-        >
-          {ColorsList.map((color) => {
-            return (
-              <TouchableOpacity key={color} onPress={() => addRow(color)}>
-                <View
-                  style={{
-                    height: 30,
-                    backgroundColor: color,
-                    width: 30,
-                    borderRadius: 20,
-                    margin: 8,
-                  }}
-                />
-              </TouchableOpacity>
-            );
-          })}
+              );
+            })}
+          </View>
         </View>
       </View>
-    </View>
+    </>
   );
 };
 
--- Items.js
+++ Items.js
@@ -1,9 +1,88 @@
 import React from 'react';
+import {
+  ScrollView,
+  View,
+  Image,
+  Text,
+  useWindowDimensions,
+} from 'react-native';
+const img1 = require('./images/fashion_image_1.jpeg');
+const img2 = require('./images/fashion_image_2.jpeg');
+const img3 = require('./images/fashion_image_3.jpeg');
+
+const AllItems = [
+  {
+    title: 'V Neck Shirt - Pink',
+    price: '$24.99',
+    image: img1,
+  },
+  {
+    title: 'V Neck Shirt - Green',
+    price: '$24.99',
+    image: img2,
+  },
+  {
+    title: 'V Neck Shirt - Grey',
+    price: '$24.99',
+    image: img3,
+  },
+  {
+    title: 'V Neck Shirt - Red',
+    price: '$24.99',
+    image: img1,
+  },
+  {
+    title: 'V Neck Shirt - Yellow',
+    price: '$24.99',
+    image: img2,
+  },
+];
 
 export default Items = () => {
+  const { width } = useWindowDimensions();
+  let defaultGroup = 2;
+  if (width > 480) defaultGroup = 3;
+  if (width > 768) defaultGroup = 4;
+  if (width > 1024) defaultGroup = 5;
+  const rows = AllItems.reduce((r, key, index) => {
+    return (
+      (index % defaultGroup == 0 ? r.push([key]) : r[r.length - 1].push(key)) &&
+      r
+    );
+  }, []);
   return (
-    <View style={{ flex: 1, margin: 10, padding: 20 }}>
-      <ScrollView></ScrollView>
+    <View style={{ flex: 1, margin: 10, padding: 10 }}>
+      <ScrollView>
+        {rows.map((row, i) => {
+          return (
+            <View
+              key={i}
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}
+            >
+              {row.map((item) => {
+                return (
+                  <View
+                    style={{
+                      backgroundColor: 'white',
+                      margin: 10,
+                      padding: 10,
+                      width: 160,
+                    }}
+                    key={item.title}
+                  >
+                    <Image
+                      style={{ width: 120, height: 120 }}
+                      source={item.image}
+                    />
+                    <Text style={{ marginTop: 10 }}>{item.title}</Text>
+                    <Text style={{ marginTop: 10 }}>{item.price}</Text>
+                  </View>
+                );
+              })}
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 };
